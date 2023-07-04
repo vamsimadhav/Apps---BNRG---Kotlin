cb685e6fef6fb8aef07712f3a07d7698d269fa8d
vamsimadhav <vamsimadhav2000@gmail.com>
2023-07-04 09:38:01 +0530

    Completed Chapter One with Challenge

--- GeoQuiz/app/src/main/java/com/vmh/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/vmh/geoquiz/MainActivity.kt
@@ -2,10 +2,53 @@ package com.vmh.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: MaterialButton
+    private lateinit var falseButton: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.trueButton)
+        falseButton = findViewById(R.id.falseButton)
+
+        trueButton.setOnClickListener{ view: View ->
+            Toast.makeText(
+                    view.context,
+                R.string.true_correct,
+                    Toast.LENGTH_SHORT
+            ).show()
+
+            //TODO: Use Snackbar Instead of Toast
+//            Snackbar.make(
+//                this,
+//                trueButton,
+//                "Correct",
+//                BaseTransientBottomBar.LENGTH_SHORT
+//            ).show()
+        }
+
+        falseButton.setOnClickListener{ view: View ->
+            Toast.makeText(
+                view.context,
+                R.string.false_incorrect,
+                Toast.LENGTH_SHORT
+            ).show()
+
+        //TODO: Use Snackbar Instead of Toast
+//            Snackbar.make(
+//                this,
+//                falseButton,
+//                "Incorrect",
+//                BaseTransientBottomBar.LENGTH_SHORT
+//            ).show()
+        }
     }
 }
